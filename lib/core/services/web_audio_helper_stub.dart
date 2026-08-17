c81092afdce6f847031e0e496ae0ef6cc4481df9
Srinjoyee_Dey <deysrinjoyee28@gmail.com>
2026-08-17 19:43:47 +0530

    Parent dashboard ui

--- lib/core/services/web_audio_helper_stub.dart
+++ lib/core/services/web_audio_helper_stub.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/foundation.dart';
+
 Future<bool> startWebRecordingImpl() async => false;
 Future<Map<String, dynamic>?> stopWebRecordingImpl() async => null;
 String? captureWebFrameImpl() => null;
 Future<bool> ensureWebCameraReadyImpl() async => false;
+void speakWebTextImpl(String text, VoidCallback onEnded) {}
+void cancelWebSpeechImpl() {}
+void playWebAudioSourceImpl(String src, VoidCallback onEnded) {}
+
